bae1905d9a862f1587f7a868e6cdd14b98ab707f
Asher <grit.developer.asher@gmail.com>
2021-10-13 12:57:54 +0900

    추가-라우터를 연결

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+
+import { history } from '../redux/configureStore';
+import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
+import Detail from '../pages/Detail';
+import Header from '../components/Header';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <ConnectedRouter history={history}>
+        <Route path='/signin' exact component={SignIn} />
+        <Route path='/signup' exact component={SignUp} />
+        <Route path='/detail' exact component={Detail} />
+      </ConnectedRouter>
+    </>
+  );
 }
 
 export default App;
