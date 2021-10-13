aa9ec3b870516c6136b78d74430294314baff3c2
Asher <grit.developer.asher@gmail.com>
2021-10-13 12:54:05 +0900

    추가-리덕스 스토어

---
+++ src/redux/configureStore.js
@@ -0,0 +1,33 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+import { connectRouter } from 'connected-react-router';
+import thunk from 'redux-thunk';
+
+// modules
+import User from './modules/user';
+
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  user: User,
+  router: connectRouter(history),
+});
+
+const middlewares = [thunk.withExtraArgument({ history })];
+
+const env = process.env.NODE_ENV;
+if (env === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+let store = initialStore => createStore(rootReducer, enhancer);
+
+export default store();
