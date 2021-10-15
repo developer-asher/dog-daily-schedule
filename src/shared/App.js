de87e28d8f956b2ca7a6c61d821583634da63606
Asher <grit.developer.asher@gmail.com>
2021-10-15 16:58:57 +0900

    수정-컴포넌트들 라우트로 묶어줌

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,24 +1,47 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
+import styled from 'styled-components';
 
 import { history } from '../redux/configureStore';
+import { userActions } from '../redux/modules/user';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
 import Detail from '../pages/Detail';
-import Header from '../components/Header';
+import Write from '../pages/Write';
+import PostList from '../pages/PostList';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import authHeader from './authHeader';
 
 function App() {
+  const dispatch = useDispatch();
+  const token = sessionStorage.getItem('auth');
+
+  useEffect(() => {
+    if (token) {
+      dispatch(userActions.signinCheckDB());
+    }
+  }, []);
+
   return (
-    <>
-      <Header></Header>
+    <Container>
       <ConnectedRouter history={history}>
+        <Route path='/' exact component={PostList} />
+        <Route path='/write' exact component={Write} />
+        <Route path='/detail/:id' exact component={Detail} />
         <Route path='/signin' exact component={SignIn} />
         <Route path='/signup' exact component={SignUp} />
         <Route path='/detail' exact component={Detail} />
       </ConnectedRouter>
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
 
 export default App;
