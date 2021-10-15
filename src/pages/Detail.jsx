720312e2a7eff33c53427f4b7cbc0f01cb9dbbb2
Asher <grit.developer.asher@gmail.com>
2021-10-15 16:33:06 +0900

    수정-게시글  id에 따른 상세 페이지

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,78 +1,92 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
+import BackspaceIcon from '@mui/icons-material/Backspace';
 
-import { commentActions } from '../redux/modules/comment';
 import Post from '../components/Post';
-import Comment from '../components/Comment';
-import ButtonEle from '../elements/ButtonEle';
-import InputEle from '../elements/InputEle';
-import LabelEle from '../elements/LabelEle';
+import CommentList from '../components/CommentList';
+import FlexWrapEle from '../elements/FlexWrapEle';
 
 const Detail = props => {
-  const dispatch = useDispatch();
-  const [input, setInput] = useState({ comment: '' });
-  const { comment } = input;
+  const { history } = props;
+  const id = props.match.params.id;
+  const pathname = props.location.pathname;
 
-  const handleChange = e => {
-    const { value, name } = e.target; //event target name
-
-    setInput({
-      [name]: value,
-    });
-  };
-
-  const writeComment = e => {
-    e.preventDefault();
-    dispatch(commentActions.setCommentFB('작성자', comment));
-    setInput({
-      comment: '',
-    });
-  };
+  const post = [
+    {
+      id: '123',
+      username: '조성민',
+      date: '2021-10-11 15:51',
+      title: '댕댕이의 하루일과',
+      image_url:
+        'https://t1.daumcdn.net/liveboard/holapet/0e5f90af436e4c218343073164a5f657.JPG',
+      content: '안녕',
+    },
+    {
+      id: '345',
+      username: '박시준',
+      date: '2021-10-14 09:00',
+      title: '댕댕이의 하루일과',
+      image_url:
+        'https://t1.daumcdn.net/liveboard/holapet/0e5f90af436e4c218343073164a5f657.JPG',
+      content: '반가워',
+    },
+    {
+      id: '567',
+      username: '이민국',
+      date: '2021-10-12 10:51',
+      title: '댕댕이의 하루일과',
+      image_url:
+        'https://t1.daumcdn.net/liveboard/holapet/0e5f90af436e4c218343073164a5f657.JPG',
+      content: '테스트중이야',
+    },
+  ];
+  const sort_post = post.filter(item => {
+    return item.id === id;
+  });
 
   return (
-    <>
-      <Post />
-      <Form>
-        <InputBox>
-          <LabelEle htmlFor='' hidden>
-            댓글작성
-          </LabelEle>
-          <InputEle
-            name='comment'
-            value={comment}
-            type='text'
-            placeholder='댓글을 작성해주세요.'
-            onChange={handleChange}
-          />
-        </InputBox>
-        <ButtonEle onClick={writeComment}>댓글작성</ButtonEle>
-      </Form>
-      <CommentList>
-        <Comment />
-      </CommentList>
-    </>
+    <Container>
+      <FlexWrapEle //
+        align_items='flex-start'
+        justify='space-between'
+        style={{
+          position: 'relative',
+        }}
+      >
+        {pathname.includes('detail') ? ( //
+          <Post detail {...sort_post[0]} />
+        ) : (
+          <></>
+        )}
+        <CommentList id={id} />
+        <BackspaceIcon
+          style={{
+            position: 'absolute',
+            right: '0',
+            top: '-40px',
+            fontSize: '40px',
+            color: '#6889b6',
+            cursor: 'pointer',
+          }}
+          onClick={() => {
+            history.push('/');
+          }}
+        />
+      </FlexWrapEle>
+    </Container>
   );
 };
-
-const Form = styled.form`
+const Container = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
 
   & > div {
-    width: 80%;
-    margin-right: 5%;
+    border-radius: 20px;
+    padding: 20px;
+    background-color: #dbd5ce;
   }
-  & input {
-    width: 100%;
-  }
-`;
-
-const InputBox = styled.div``;
-
-const CommentList = styled.ul`
-  padding-left: 0;
-  list-style: none;
 `;
 
 export default Detail;
