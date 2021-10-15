f6556a46006722e59263550b83a62f20311e6d7f
Asher <grit.developer.asher@gmail.com>
2021-10-15 16:35:28 +0900

    수정-로그인페이지 css

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 
+import ButtonEle from '../elements/ButtonEle';
+import InputEle from '../elements/InputEle';
+import LabelEle from '../elements/LabelEle';
 import { userActions } from '../redux/modules/user';
 
+// icon style
+const icon_login = {
+  position: 'absolute',
+  left: '5px',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  color: '#555',
+};
+// btn style
+const btn_login = {
+  width: '100%',
+  marginTop: '40px',
+};
+
 const SignIn = props => {
+  const { history } = props;
   const dispatch = useDispatch();
   const [input, setInput] = useState({ id: '', pwd: '' });
   const { id, pwd } = input;
@@ -17,43 +38,107 @@ const SignIn = props => {
     });
   };
 
-  const signIn = e => {
+  const handleClick = e => {
     e.preventDefault();
 
-    dispatch(userActions.signinFB(id, pwd));
-    setInput({
-      id: '',
-      pwd: '',
-    });
+    if (e.target.textContent === '로그인') {
+      dispatch(userActions.signinDB(id, pwd));
+      setInput({
+        id: '',
+        pwd: '',
+      });
+    }
+
+    if (e.target.textContent === '회원가입') {
+      history.push('/signup');
+    }
   };
 
   return (
-    <>
-      <form>
-        <p>
-          <label htmlFor=''>아이디</label>
-          <input
+    <SignBox>
+      <SignTitle>멍일과</SignTitle>
+      <SigninForm>
+        <InputBox>
+          <PersonOutlineIcon style={icon_login} />
+          <LabelEle hidden htmlFor='id'>
+            아이디
+          </LabelEle>
+          <InputEle
+            id='id'
             name='id'
             value={id}
             type='text'
-            placeholder='id'
+            placeholder='아이디'
             onChange={handleChange}
           />
-        </p>
-        <p>
-          <label htmlFor=''>비밀번호</label>
-          <input
+        </InputBox>
+        <InputBox>
+          <LockOpenIcon style={icon_login} />
+          <LabelEle hidden htmlFor='pwd'>
+            비밀번호
+          </LabelEle>
+          <InputEle
+            id='pwd'
             name='pwd'
             value={pwd}
             type='password'
-            placeholder='password'
+            placeholder='비밀번호'
             onChange={handleChange}
           />
-        </p>
-        <button onClick={signIn}>로그인</button>
-      </form>
-    </>
+        </InputBox>
+        <ButtonWrap onClick={handleClick}>
+          <ButtonEle style={btn_login}>로그인</ButtonEle>
+          <ButtonEle style={{ ...btn_login, marginTop: '5px' }}>
+            회원가입
+          </ButtonEle>
+        </ButtonWrap>
+      </SigninForm>
+    </SignBox>
   );
 };
+
+const SignBox = styled.div`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  width: 400px;
+  min-height: 250px;
+  transform: translateX(-50%) translateY(-50%);
+`;
+
+const SignTitle = styled.h2`
+  text-align: center;
+  font-family: 'Gugi', cursive;
+  font-size: 30px;
+  color: #9dc8f1;
+`;
+
+const SigninForm = styled.form`
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 30px;
+  box-shadow: 0 2px 6px 0 rgb(68 68 68 / 8%);
+`;
+
+const InputBox = styled.p`
+  position: relative;
+  margin: 0;
+
+  & > input {
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 0 0 5px 5px;
+    padding-left: 30px;
+    outline-color: #9dc8f1;
+    font-size: 15px;
+  }
+
+  &:first-child > input {
+    border-bottom: none;
+    border-radius: 5px 5px 0 0;
+  }
+`;
+
+const ButtonWrap = styled.div``;
 
 export default SignIn;
